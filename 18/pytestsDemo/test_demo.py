aedde898ffcf026c82b6fe32c985b5499a57105d
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-12-14 23:42:35 +0200

    Add initial Pytest scripts for basic Selenium WebDriver tests with assertions

---
+++ 18/pytestsDemo/test_demo.py
@@ -0,0 +1,36 @@
+# Any Pytest file should start with test_ or end with _test
+# Pytest method names should start with test
+# Any code should be wrapped in method only
+# To run tests, go to terminal and type pytest -v -s
+# -v is for verbose, -s is for console output
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+service_obj = Service("/Users/lironefitoussi/Developer/PySel/chromedriver")
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+
+
+options.add_argument("--ignore-certificate-errors")  # Ignore SSL errors
+options.add_argument("window-size=1400,800")
+
+driver = webdriver.Chrome(service=service_obj, options=options)
+
+driver.implicitly_wait(5)
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+
+def test_first_program():
+    print("Hello World")
+    assert driver.title == "ProtoCommerce", "Title is not matching" # This will pass
+
+def test_second_program_alpha():
+    print("Hello World 2")
+    assert driver.title == "ProtoCommerce - ProtoCommerce", "Title is not matching" # This will fail
+    
+# used Flag -s to see the print statements in the console
+# used Flag -v to see the verbose output of the test cases
---
+++ 18/pytestsDemo/test_demo_b.py
@@ -0,0 +1,9 @@
+
+def test_third_program():
+    msg = "Hello World"
+    assert msg == "Hello Bro", "Message is not matching" # This will fail
+
+def test_fourth_program_alpha():
+    a = 4
+    b = 6
+    assert a+2 == 6, "Addition is not matching" # This will pass
